e05d7ba59207e7b45a4b5a6730d6086ee46d2340
Luna R <snowluna@protonmail.com>
2022-07-18 06:46:37 +0000

    add datepicker and color backgrounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+   // get date from task element
+   var date = $(taskEl).find("span").text().trim();
+ 
+   // convert to moment object at 5:00pm
+   var time = moment(date, "L").set("hour", 17);
+
+   // remove any old classes from element
+   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+   // apply new class if task is near/over due date
+   if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+   }
+   else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+   }
+   // this should print out an object for the value of the date variable, but at 5:00pm of that date
+   console.log(time);
+ 
+ };
+
 var createTask = function (taskText, taskDate, taskList) {
    // create elements that make up a task item
    var taskLi = $("<li>").addClass("list-group-item");
@@ -10,6 +32,9 @@ var createTask = function (taskText, taskDate, taskList) {
 
    // append span and p element to parent li
    taskLi.append(taskSpan, taskP);
+
+   // check due date
+   auditTask(taskLi);
 
    // append to ul list on the page
    $("#list-" + taskList).append(taskLi);
@@ -30,7 +55,6 @@ var loadTasks = function () {
 
    // loop over object properties
    $.each(tasks, function (list, arr) {
-      console.log(list, arr);
       // then loop over sub-array
       arr.forEach(function (task) {
          createTask(task.text, task.date, list);
@@ -121,12 +145,21 @@ $(".list-group").on("click", "span", function () {
       .val(date);
    $(this).replaceWith(dateInput);
 
+   // enable jquery ui datepicker
+   dateInput.datepicker({
+      minDate: 1,
+      onClose: function() {
+         // when calendar is closed, force a "change" event on dateInput
+         $(this.trigger("change"));
+      }
+   });
+
    // automatically bring up the calendar
    dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
    var date = $(this).val();
 
    // get status type and position in the list
@@ -142,6 +175,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
       .addClass("badge badge-primary badge-pill")
       .text(date);
    $(this).replaceWith(taskSpan);
+
+   // Pass task's <li> element into auditTask() to check new due date
+   auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -212,6 +248,10 @@ $("#trash").droppable({
    out: function (event, ui) {
       console.log("out");
    },
+});
+
+$("#modalDueDate").datepicker({
+   minDate: 1
 });
 
 // load tasks for the first time
